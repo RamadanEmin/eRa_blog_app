b2babf9ac4eba02faa1a412066a793dec5587981
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-16 19:48:14 +0200

    Added AuthLinks component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './navbar.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import AuthLinks from '../authLinks/AuthLinks';
 
 const Navbar = () => {
     return (
@@ -17,6 +18,7 @@ const Navbar = () => {
                 <Link href="/" className={styles.link}>Homepage</Link>
                 <Link href="/" className={styles.link}>Contact</Link>
                 <Link href="/" className={styles.link}>About</Link>
+                <AuthLinks/>
             </div>
         </div>
     );
