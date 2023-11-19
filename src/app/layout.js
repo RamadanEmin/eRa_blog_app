3e5c9253d11b4b5ed90c84521952c2c6838bd42b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-19 21:33:17 +0200

    Added 'Footer' component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
 import ThemeProvider from '@/providers/ThemeProvider';
 import { ThemeContextProvider } from '@/context/ThemeContext';
 
@@ -16,16 +17,17 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <ThemeContextProvider>
-                    <ThemeProvider>
-                        <div className="container">
-                            <div className="wrapper">
-                                <Navbar />
-                                {children}
+                    <ThemeContextProvider>
+                        <ThemeProvider>
+                            <div className="container">
+                                <div className="wrapper">
+                                    <Navbar />
+                                    {children}
+                                    <Footer />
+                                </div>
                             </div>
-                        </div>
-                    </ThemeProvider>
-                </ThemeContextProvider>
+                        </ThemeProvider>
+                    </ThemeContextProvider>
             </body>
         </html>
     );
