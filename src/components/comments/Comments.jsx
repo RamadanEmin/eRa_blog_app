a9545d3f3212208dca410a39da2c7c8632799cf9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-11-22 21:42:59 +0200

    Update Comments component to fetch and post comment

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,81 +1,71 @@
-import Link from 'next/link';
-import styles from './comments.module.css';
-import Image from 'next/image';
+'use client';
 
-const Comments = () => {
-    const status = 'authenticated';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import Image from 'next/image';
+import useSWR from 'swr';
+
+import styles from './comments.module.css';
+
+const fetcher = async (url) => {
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        const error = new Error(data.message);
+        throw error;
+    }
+
+    return data;
+};
+
+const Comments = ({ postSlug }) => {
+    const { status } = useSession();
+
+    const [des, setDesc] = useState('');
+
+    const { data, mutate, isLoading } = useSWR(`http://localhost:3000/api/comments?postSlug=${postSlug}`, fetcher);
+
+    const handleSubmit = async () => {
+        await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ des, postSlug })
+        });
+
+        mutate();
+    };
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Comments</h1>
             {status === 'authenticated' ? (
                 <div className={styles.write}>
-                    <textarea placeholder="write a comment..." className={styles.input}></textarea>
-                    <button className={styles.button}>Send</button>
+                    <textarea
+                        placeholder="write a comment..."
+                        className={styles.input}
+                        onChange={(e) => setDesc(e.target.value)}
+                    >
+                    </textarea>
+                    <button className={styles.button} onClick={handleSubmit}>Send</button>
                 </div>
             ) : (
                 <Link href="/login">Login to write a comment</Link>
             )}
             <div className={styles.comments}>
-                <div className={styles.comment}>
-                    <div className={styles.user}>
-                        <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                        <div className={styles.userInfo}>
-                            <span className={styles.username}>John Doe</span>
-                            <span className={styles.date}>13.11.2023</span>
+                {isLoading ? 'Loading...' : data?.map(item => (
+                    <div className={styles.comment} key={item._id}>
+                        <div className={styles.user}>
+                            {item?.user?.image && <Image src={item.user.image} alt="" width={50} height={50} className={styles.image} />}
+                            <div className={styles.userInfo}>
+                                <span className={styles.username}>{item.user.name}</span>
+                                <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
+                            </div>
                         </div>
+                        <p className={styles.desc}>{item.des}</p>
                     </div>
-                    <p className={styles.desc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro est tempora,
-                        excepturi omnis fugiat in molestiae ad et tempore ab tenetur numquam beatae
-                        temporibus odit soluta odio culpa magnam reiciendis.
-                    </p>
-                </div>
-
-                <div className={styles.comment}>
-                    <div className={styles.user}>
-                        <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                        <div className={styles.userInfo}>
-                            <span className={styles.username}>John Doe</span>
-                            <span className={styles.date}>13.11.2023</span>
-                        </div>
-                    </div>
-                    <p className={styles.desc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro est tempora,
-                        excepturi omnis fugiat in molestiae ad et tempore ab tenetur numquam beatae
-                        temporibus odit soluta odio culpa magnam reiciendis.
-                    </p>
-                </div>
-
-                <div className={styles.comment}>
-                    <div className={styles.user}>
-                        <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                        <div className={styles.userInfo}>
-                            <span className={styles.username}>John Doe</span>
-                            <span className={styles.date}>13.11.2023</span>
-                        </div>
-                    </div>
-                    <p className={styles.desc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro est tempora,
-                        excepturi omnis fugiat in molestiae ad et tempore ab tenetur numquam beatae
-                        temporibus odit soluta odio culpa magnam reiciendis.
-                    </p>
-                </div>
-
-                <div className={styles.comment}>
-                    <div className={styles.user}>
-                        <Image src="/p1.jpeg" alt="" width={50} height={50} className={styles.image} />
-                        <div className={styles.userInfo}>
-                            <span className={styles.username}>John Doe</span>
-                            <span className={styles.date}>13.11.2023</span>
-                        </div>
-                    </div>
-                    <p className={styles.desc}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro est tempora,
-                        excepturi omnis fugiat in molestiae ad et tempore ab tenetur numquam beatae
-                        temporibus odit soluta odio culpa magnam reiciendis.
-                    </p>
-                </div>
+                ))}
             </div>
         </div >
     );
